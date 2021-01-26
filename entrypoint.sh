230c0cae5ed7a7afd3299aad06efb9ee4e13f862
Aaftakhov <simple1elf@gmail.com>
2021-01-26 11:37:41 +0500

    fix launch and job name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-allurectl upload ./${INPUT_ALLURE_RESULTS} --ci-type github -e ${INPUT_ALLURE_EE_ENDPOINT} -t ${INPUT_ALLURE_EE_TOKEN} --project-id ${INPUT_PROJECT_ID} --job-name "GITHUB_ACTIONS" --job-run-name ${INPUT_LAUNCH_NAME} --job-run-uid ${INPUT_JOB_RUN_UID} --launch-name ${INPUT_LAUNCH_NAME}
+allurectl upload ./${INPUT_ALLURE_RESULTS} --ci-type github -e ${INPUT_ALLURE_EE_ENDPOINT} -t ${INPUT_ALLURE_EE_TOKEN} --project-id ${INPUT_PROJECT_ID} --job-name ${INPUT_JOB_NAME} --job-run-name ${INPUT_LAUNCH_NAME} --launch-name ${INPUT_LAUNCH_NAME} --job-run-uid ${INPUT_JOB_RUN_UID}
